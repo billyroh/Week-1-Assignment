30d897bc820c44ee35f5959c3e7259cacd2db949
Billy Roh <billy.roh@gmail.com>
2014-06-11 16:33:28 -0700

    Initial Commit

---
+++ Week 1 AssignmentTests/Week_1_AssignmentTests.m
@@ -0,0 +1,34 @@
+//
+//  Week_1_AssignmentTests.m
+//  Week 1 AssignmentTests
+//
+//  Created by Billy Roh on 6/11/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week_1_AssignmentTests : XCTestCase
+
+@end
+
+@implementation Week_1_AssignmentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
